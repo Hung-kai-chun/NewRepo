ea52e4d33c23715d2967c03012f68ced033db4bf
Hung-kai-chun <a940910b@gmail.com>
2026-09-24 16:59:44 +0800

    P_34

---
+++ P_34.kt
@@ -0,0 +1,21 @@
+﻿fun main() {
+    val num1 = 10
+    val num2 = 5
+
+    println("num1和num2的各種運算:")
+    println("num1+num2等於${num1 + num2}")
+    println("num1-num2等於${num1 - num2}")
+    println("num1*num2等於${num1 * num2}")
+    println("num1/num2等於${num1 / num2}")
+    println("num1%num2等於${num1 % num2}")
+
+    var a = 0
+    var b = 0
+    var c = 0
+
+    b = a++
+    c = ++a
+
+    println("因為是在指定值之後才遞增,所以b的值為$b")
+    println("因為是在遞增之後才指定值,所以c的值為$c")
+}
