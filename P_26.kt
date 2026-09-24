9ac7a4700211552460264913a89a31bdc0c5995d
Hung-kai-chun <a940910b@gmail.com>
2026-09-24 16:49:39 +0800

    P_26

---
+++ P_26.kt
@@ -0,0 +1,10 @@
+﻿fun main() {
+    var num = 3 // 宣告變數 num 並給予初始值 3（Kotlin 會自動推導型態為 Int）
+
+    println("變數num的值是$num")
+
+    num = 5 // 重新指定變數 num 的值為 5
+
+    println("更新變數num的值")
+    println("變數num更新後的值是$num")
+}
