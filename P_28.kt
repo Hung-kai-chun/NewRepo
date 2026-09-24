89e09202aa6badb90179c4ba5817d1af72945aab
Hung-kai-chun <a940910b@gmail.com>
2026-09-24 16:55:33 +0800

    P_28

---
+++ P_28.kt
@@ -0,0 +1,15 @@
+﻿fun main() {
+    println("請輸入一個整數")
+
+    // Kotlin 內建 readln()，不需要建立 BufferedReader
+    val str1 = readln()
+    val num = str1.toInt()
+
+    println("您輸入的數字是:$num")
+
+    println("請輸入字串")
+
+    val str2 = readln()
+
+    println("剛剛輸入的字串是:$str2")
+}
