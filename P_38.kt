d177b809383f0ef173f7588dd4e436404b429daa
Hung-kai-chun <a940910b@gmail.com>
2026-09-24 17:01:11 +0800

    P_38

---
+++ P_38.kt
@@ -0,0 +1,12 @@
+﻿fun main() {
+    val dnum = 160.5
+
+    println("身高是${dnum}公分")
+
+    println("指定給int型態的變數")
+
+    // Kotlin 不支援 C 語言風格的 (int)dnum 轉型，必須使用 .toInt() 方法
+    val inum = dnum.toInt()
+
+    println("身高是${inum}公分")
+}
