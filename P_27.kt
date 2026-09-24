65de9c2f07826e0583a5cbf17bce7edbcee4cb3f
Hung-kai-chun <a940910b@gmail.com>
2026-09-24 16:51:28 +0800

    P_27

---
+++ P_27.kt
@@ -0,0 +1,13 @@
+﻿fun main() {
+    var num1: Int // 宣告變數 num1
+    var num2: Int // 宣告變數 num2
+
+    num1 = 3
+
+    println("變數num1的值是:$num1")
+
+    num2 = num1   // 將變數 num1 的值指定給變數 num2
+
+    println("將變數num1指定到變數num2之中")
+    println("變數num2的值是:$num2")
+}
